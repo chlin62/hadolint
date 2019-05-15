65edef6fd38977f375a13f9ae0798a590e1d4707
chlin62 <chlin62@outlook.com>
2019-05-15 10:57:58 +0800

    Update Rules.hs

--- src/Hadolint/Rules.hs
+++ src/Hadolint/Rules.hs
@@ -800,3 +800,12 @@ registryIsAllowed allowed = instructionRuleState code severity message check Set
     isAllowed Image {registryName = Nothing, imageName} =
         imageName == "scratch" ||
         Set.member "docker.io" allowed || Set.member "hub.docker.com" allowed
+   noApkUpgrade :: Rule
+
+noApkUpgrade = instructionRule code severity message check
+  where
+    code = "TSMC1001"
+    severity = ErrorC
+    message = "Do not use yum command"
+    check (Run args) = argumentsRule (Shell.noCommands (Shell.cmdHasArgs "apk" )) args
+    check _ = True
